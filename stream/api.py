96cdf7c113687ccc15642eb63ae87035812016e2
Karel Peeters <karel.peeters.leuven@gmail.com>
2023-03-13 20:14:17 +0100

    Reset api to original, split out edited file.

--- stream/api.py
+++ stream/api.py
@@ -1,25 +1,9 @@
-import re
-from typing import Generator
-
-import networkx
-from matplotlib import pyplot as plt
 from zigzag.classes.stages import *
-
 from stream.classes.stages import *
-from stream.classes.workload.computation_node import ComputationNode
+import re
 
 
-class DebugStage(Stage):
-    def run(self) -> Generator:
-        kwargs = self.kwargs.copy()
-        save_graph(self.kwargs["workload"], f"outputs/graph.svg", all_ranges=True)
-
-        sub_stage = self.list_of_callables[0](self.list_of_callables[1:], **kwargs)
-        for i, (cme, extra_info) in enumerate(sub_stage.run()):
-            yield cme, extra_info
-
-
-def get_hardware_performance_stream(hardware, workload, mapping, CN_define_mode, hint_loops):
+def get_hardware_performance_stream(hardware, workload, mapping, CN_define_mode, hint_loops, node_hw_cost_pkl_name):
 
     # Initialize the logger
     import logging as _logging
@@ -42,8 +26,8 @@ def get_hardware_performance_stream(hardware, workload, mapping, CN_define_mode,
         workload_path=workload,  # required by ModelParserStage
         mapping_path=mapping,  # required by ModelParserStage
         loma_lpf_limit=6,  # required by LomaStage
-        nb_ga_individuals=4,  # number of individuals in each genetic algorithm generation
-        nb_ga_generations=1,  # number of genetic algorithm generations
+        nb_ga_individuals=128,  # number of individuals in each genetic algorithm generation
+        nb_ga_generations=100,  # number of genetic algorithm generations
         node_hw_performances_path=f"outputs/{node_hw_cost_pkl_name}.pickle",  # saved node_hw_performances to skip re-computation
         plot_hof=True,  # Save schedule and memory usage plot of each individual in the Genetic Algorithm hall of fame
         plot_file_name=True,
@@ -59,48 +43,18 @@ def get_hardware_performance_stream(hardware, workload, mapping, CN_define_mode,
     return answers
 
 
-def save_graph(graph, path: str, all_ranges: bool):
-    for (node, data) in graph.nodes(data=True):
-        node: ComputationNode
-        rows = ""
-
-        rows += f'<tr><td colspan="3"><b>{node}</b></td></tr>'
-        rows += f'<tr><td colspan="3">{type(node).__name__}</td></tr>'
-        rows += f"<tr><td>time</td><td>{node.start}</td><td>{node.end}</td></tr>"
-        for axis, (start, end) in node.loop_ranges.items():
-            if all_ranges:
-                plot_range = True
-            else:
-                const = True
-                for (other, other_data) in graph.nodes(data=True):
-                    if axis in other.loop_ranges and other.loop_ranges[axis] != (start, end):
-                        const = False
-                        break
-                plot_range = not const
-
-            if plot_range:
-                rows += f"<tr><td>{axis}</td><td>{start}</td><td>{end}</td></tr>"
-
-        data["label"] = f'<<table border="0">{rows}</table>>'
-
-    graph_dot = networkx.drawing.nx_pydot.to_pydot(graph)
-
-    print(f"Writing to {path}")
-    with open(path, "wb") as f:
-        f.write(graph_dot.create_svg())
-
-
 if __name__ == "__main__":
     # accelerator = 'inputs.examples.hardware.TPU_like_quad_core'
     # workload = 'inputs.examples.workload.resnet18_few_layers'
     # mapping = 'inputs.examples.mapping.tpu_like_quad_core'
 
-    accelerator = 'inputs.testing.hardware.quad_testing_core_offchip'
-    workload = 'inputs.testing.workload.testing_workload_for_4_cores'
+    CN_define_mode = 1
+    hint_loops = [('OY', 'all')]
+
+    accelerator = 'inputs.testing.hardware.dual_testing_core_offchip'
+    workload = 'inputs.testing.workload.testing_workload_for_2_cores'
     mapping = 'inputs.testing.mapping.testing_mapping'
 
-    CN_define_mode = 1  # manually define outer CN size for all cores and all layers
-    hint_loops = [('OY', 'all')]
     # hint_loops = []
 
     hw_name = accelerator.split(".")[-1]
@@ -108,7 +62,7 @@ if __name__ == "__main__":
     experiment_id = f"{hw_name}-{wl_name}-CNmode_{CN_define_mode}-hintloop_{str(hint_loops)}"
     node_hw_cost_pkl_name = f'saved_CN_HW_cost-{experiment_id}'
 
-    scme, _ = get_hardware_performance_stream(accelerator, workload, mapping, CN_define_mode, hint_loops)
+    scme, _ = get_hardware_performance_stream(accelerator, workload, mapping, CN_define_mode, hint_loops, node_hw_cost_pkl_name)
 
     from stream.visualization.schedule import plot_timeline_brokenaxes
     from stream.visualization.memory_usage import plot_memory_usage
